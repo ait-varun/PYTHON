fbb8fac55ed4e9bc3fd0691d694ac1d2968116c0
Varun <aitvarunbhatia@gmail.com>
2024-08-06 13:05:58 +0530

    added sets

---
+++ sets.py
@@ -0,0 +1,26 @@
+# Create a empty set
+s = set()
+
+# Add an element to a set
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(2)
+s.add(1)
+
+print(s)
+
+# {1, 2, 3}
+# {1, 2, 3}
+
+# Remove an element from a set
+# s.remove(2)
+
+
+len(s)
+
+# gives the number of elements in the set
+
+print(f"The set contains {len(s)} elements")
+
+# The set contains 3 elements
