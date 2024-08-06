0cd55496ab3498bd6665938d745035ac72c33d47
Varun <aitvarunbhatia@gmail.com>
2024-08-06 12:43:08 +0530

    initial commit

---
+++ conditions.py
@@ -0,0 +1,18 @@
+# 1. if statement
+num = int(input("Number: "))
+if (num % 2) == 0:
+    print("{} is even".format(num))
+else:
+    print("{} is odd".format(num))
+
+# 2. if-else statement
+
+    n = int(input("Enter a number: "))
+
+if (n>0):
+  print("n is positive")
+elif n<0:
+  print("n is not positive")
+else:
+  print("n is zero")
+
---
+++ hello.py
@@ -0,0 +1,12 @@
+# 1. print statement
+name = "Varun"
+print("Hello World, this is " + name)
+
+# 2. input statement
+name = input("Enter your name: ")
+print("Hello, " + name)
+
+#3. f for string formatting
+
+name = input("Enter your name: ")
+print(f"Hello, {name}")
