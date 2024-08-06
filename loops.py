05b8f16f3e8775442abadf76b227af316e673c2b
Varun <aitvarunbhatia@gmail.com>
2024-08-06 13:09:39 +0530

    added loops

---
+++ loops.py
@@ -0,0 +1,19 @@
+for i in [0, 1, 2, 3,4,5]:
+    print(i)
+
+
+
+
+for i in range(0, 6):
+    print(i)
+#prints 0,1,2,3,4,5
+
+names = ["Harry", "Ron", "Hermione", "Hermione", "Ron", "Harry"]
+for name in names:
+    print(name)
+#prints Harry, Ron, Hermione, Hermione, Ron, Harry
+
+name = "Harry"
+for char in name:
+  print(char)
+#prints H, a, h, r, r, y
