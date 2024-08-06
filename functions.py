4d4a4d3831fb31558822c580cae375eb23b8640c
Varun <aitvarunbhatia@gmail.com>
2024-08-06 13:15:41 +0530

    added functions

---
+++ functions.py
@@ -0,0 +1,11 @@
+# def square(x):
+#     return x * x
+
+from squares import square # importing the square function
+
+
+for i in range(10):
+    print(square(i))
+
+    # prints the squares of the numbers from 0 to 9
+
