fdf4a7c78a00a58daa6b0c70ad39162d8ea14209
Varun <aitvarunbhatia@gmail.com>
2024-08-06 14:37:29 +0530

    added functions

--- functions.py
+++ functions.py
@@ -5,7 +5,12 @@ from squares import square # importing the square function
 
 
 for i in range(10):
-    print(square(i))
+    print(f"The square of {i} is {square(i)}")
 
     # prints the squares of the numbers from 0 to 9
 
+import squares
+
+for i in range(10):
+    print(f"The square gets {i} is {squares.square(i)}")
+    # prints the squares of the numbers from 0 to 9
