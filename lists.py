c9542f9f1d7b97a36a1d74d5fc3239084ac99b82
Varun <aitvarunbhatia@gmail.com>
2024-08-06 12:59:12 +0530

    added lists

---
+++ lists.py
@@ -0,0 +1,10 @@
+# Defines a list of names
+names = ['Harry', 'Ron', 'Hermione', 'Hermione']
+
+names.append("Draco") # Adds a new name at end of list
+
+names.sort() # Sorts the list alphabetically
+
+print(names)
+
+# ['Draco', 'Harry', 'Hermione', 'Hermione', 'Ron']
